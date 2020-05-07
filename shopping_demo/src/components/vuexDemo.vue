b41e585b961b319466f583ab08e86e7062402108
ZqYuy <1842567354@qq.com>
2020-05-07 09:31:26 +0800

    one1

--- shopping_demo/src/components/vuexDemo.vue
+++ shopping_demo/src/components/vuexDemo.vue
@@ -2,6 +2,10 @@
 <div>
 <h1>{{$store.state.hello}}</h1>
 <button @click="getStoreAge">getStoreAge</button>
+<h3>{{$store.state.name}}</h3>
+<button @click="chengeName('xz')">改变name</button>
+<button @click="chengeHello('欢迎使用vuex！')">改变hello</button>
+<button @click="chengeTow('jack', '欢迎欢迎')">改变两个</button>
 </div>
 </template>
 
@@ -23,6 +27,15 @@ export default {
   methods: {
     getStoreAge: function () {
       alert(this.$store.state.age)
+    },
+    chengeName: function (newName) {
+      this.$store.commit('edit', newName)
+    },
+    chengeHello: function (newHello) {
+      this.$store.commit('chengeHello', newHello)
+    },
+    chengeTow: function (newName, newHello) {
+      this.$store.commit('setEdit', newName, newHello)
     }
   }
 }
--- shopping_demo/src/store/index.js
+++ shopping_demo/src/store/index.js
@@ -8,7 +8,23 @@ const store = new Vuex.Store({
   state: {
     hello: 'hello,vuex!',
     age: 18,
-    count: 1
+    count: 1,
+    name: 'ZqYuy'
+  },
+  mutations: {
+    // edit: function (state, newName) {
+    //   state.name = newName
+    // },
+    edit (state, newName) {
+      state.name = newName
+    },
+    chengeHello (state, newHello) {
+      state.hello = newHello
+    },
+    setEdit (state, newName, newHello) {
+      state.name = newName
+      state.hello = newHello
+    }
   }
 })
 export default store
